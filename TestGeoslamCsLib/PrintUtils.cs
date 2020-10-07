2e5381d59c0d0f7f64716396281daa06c88acdd4
Aaron.Wilson <aaron.wilson@geoslam.com>
2020-10-07 16:37:50 +0100

    added test code

---
+++ TestGeoslamCsLib/PrintUtils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGeoslamCsLib
+{
+    public class PrintCommands
+    {
+        public static void PrintGreeting()
+        {
+            Console.WriteLine("Hello world :)");
+        }
+
+        public static void PrintSum(int a, int b)
+        {
+            Console.WriteLine(string.Format("{0}", a + b));
+        }
+
+        public static void PrintProduct(int a, int b)
+        {
+            Console.WriteLine(string.Format("{0}", a * b));
+        }
+
+        public static void PrintGoodbye()
+        {
+            Console.WriteLine("Goodbye world :(");
+        }
+    }
+}
